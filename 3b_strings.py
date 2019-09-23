85c89b28c7e854b652afe90bda568ad02c05163c
pawelnaw <dverg@poczta.onet.pl>
2019-09-23 14:40:52 +0200

    name change

---
+++ 3b_strings.py
@@ -0,0 +1,26 @@
+print("This is a line of text!")
+print("Tjis is first line!\n This is second line!")
+print("This is some \" text")
+
+phrase = "String in a variable"
+print(phrase)
+print("\n" + phrase + " and concatenated string")
+
+print(phrase.capitalize())
+print(phrase.upper())
+
+print(phrase.isupper())
+print(phrase.islower())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+print(phrase{2})
+
+print(phrase.index("S"))
+print(phrase.index("i"))
+
+print(phrase)
+print(phrase.replace("variable", "variable_replaced"))
+
+.replace("value", "value")
